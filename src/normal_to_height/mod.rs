2e6b252406d9bdce50aa564bd862345ea429e5ab
miguel <vackap@gmail.com>
2025-04-28 18:24:38 -0300

    first tests

--- src/normal_to_height/mod.rs
+++ src/normal_to_height/mod.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 
-use std::path::PathBuf;
+use std::{
+    ops::{Add, Div, Mul, Sub},
+    path::PathBuf,
+};
 
 use image::{ImageBuffer, Pixel, Rgb};
 
@@ -18,9 +21,16 @@ enum Diagonal {
     DownRight,
 }
 
+fn map_range<T: Copy>(from_range: (T, T), to_range: (T, T), s: T) -> T
+where
+    T: Add<T, Output = T> + Sub<T, Output = T> + Mul<T, Output = T> + Div<T, Output = T>,
+{
+    to_range.0 + (s - from_range.0) * (to_range.1 - to_range.0) / (from_range.1 - from_range.0)
+}
+
 pub struct NormalToHeight {
     dim: (u32, u32),
-    im: ImageBuffer<Rgb<u8>, Vec<u8>>,
+    im: ImageBuffer<Rgb<f32>, Vec<f32>>,
 }
 
 impl NormalToHeight {
@@ -33,7 +43,7 @@ impl NormalToHeight {
         Ok(NormalToHeight { dim, im })
     }
 
-    fn get_direction(&self, x: u32, y: u32, direction: Direction) -> Option<&Rgb<u8>> {
+    fn get_direction(&self, x: u32, y: u32, direction: Direction) -> Option<&Rgb<f32>> {
         match direction {
             Direction::Up => {
                 if let Some(y) = y.checked_sub(1) {
@@ -55,11 +65,11 @@ impl NormalToHeight {
     }
 
     fn get_diagonal(
-        im: &ImageBuffer<Rgb<u8>, Vec<u8>>,
+        im: &ImageBuffer<Rgb<f32>, Vec<f32>>,
         x: u32,
         y: u32,
         diagonal: Diagonal,
-    ) -> Option<&Rgb<u8>> {
+    ) -> Option<&Rgb<f32>> {
         match diagonal {
             Diagonal::UpLeft => {
                 if let (Some(y), Some(x)) = (y.checked_sub(1), x.checked_sub(1)) {
@@ -85,35 +95,75 @@ impl NormalToHeight {
         }
     }
 
+    fn to_rgb(
+        mut buffer: ImageBuffer<Rgb<f32>, Vec<f32>>,
+        min: Rgb<f32>,
+        max: Rgb<f32>,
+    ) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+        for pixel in buffer.pixels_mut() {
+            let mut data = pixel.0;
+
+            for (i, v) in data.into_iter().enumerate() {
+                data[i] = map_range((min.0[i], max.0[i]), (0.0, 1.0), v)
+            }
+
+            pixel.0 = data;
+        }
+
+        ImageBuffer::from_par_fn(buffer.width(), buffer.height(), |x, y| {
+            let pixel = buffer.get_pixel(x, y);
+
+            let data = pixel.0;
+
+            Rgb([
+                (data[0] * 255.0) as u8,
+                (data[1] * 255.0) as u8,
+                (data[2] * 255.0) as u8,
+            ])
+        })
+    }
+
     pub fn execute(&self) {
-        let mut buffer: ImageBuffer<Rgb<u8>, Vec<u8>> =
+        let mut buffer: ImageBuffer<Rgb<f32>, Vec<f32>> =
             image::ImageBuffer::new(self.dim.0, self.dim.1);
+
+        let mut min_values = Rgb([0.0f32, 0.0, 0.0]);
+        let mut max_values = Rgb([0.0f32, 0.0, 0.0]);
 
         for (_, row) in self.im.enumerate_rows() {
             for (x, y, pixel) in row {
-
                 let Some(down_right) =
-                    NormalToHeight::get_diagonal(&self.im, x, y, Diagonal::DownRight)
+                    NormalToHeight::get_diagonal(&self.im, x, y, Diagonal::DownLeft)
                 else {
                     continue;
                 };
 
                 // subtract one from the other
-                let mut new = pixel.map2(down_right, |a, b| b.saturating_sub(a));
+                let mut new = pixel.map2(down_right, |a, b| b - a);
 
                 let bp = buffer.get_pixel(x, y);
 
                 if let Some(bp_up_left) =
-                    NormalToHeight::get_diagonal(&buffer, x, y, Diagonal::UpLeft)
+                    NormalToHeight::get_diagonal(&buffer, x, y, Diagonal::UpRight)
                 {
-                    new = new.map2(bp_up_left, |a, b| a.saturating_add(b));
+                    new = new.map2(bp_up_left, |a, b| a + b);
                 }
+
+                new.0.iter().enumerate().for_each(|(i, v)| {
+                    if *v > max_values.0[i] {
+                        max_values.0[i] = *v;
+                    } else if *v < min_values.0[i] {
+                        min_values.0[i] = *v;
+                    }
+                });
 
                 buffer.put_pixel(x, y, new);
             }
         }
 
-        buffer.save("buffer.png").expect("could not save buffer");
+        let buffer = NormalToHeight::to_rgb(buffer, min_values, max_values);
+
+        buffer.save("buffer_up_right.png").expect("could not save buffer");
     }
 }
 
